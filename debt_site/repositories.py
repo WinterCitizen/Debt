a21a43c447da9fa3cf527a6748dc75e097b6aed2
Vitautas Sabenin <vitautassabenin97@gmail.com>
2019-11-21 04:26:41 +0300

    Add debt repositories tests

--- debt_site/repositories.py
+++ debt_site/repositories.py
@@ -1,51 +1,44 @@
 import dataclasses
-from typing import Iterable, List
+from typing import Dict, Iterable, List, Union
 
 from debt_site.models import Debt
 
 
 class DebtRepository:
-    def create(self, first_name: str, last_name: str,
-               debt_amount: str) -> Debt:
+    def create(self, *, first_name: str, last_name: str,
+               debt_amount: int) -> Debt:
         result: Debt = Debt.objects.create(first_name=first_name,
                                            last_name=last_name,
                                            debt_amount=debt_amount)
         return result
 
-    def filter(self, first_name: str, last_name: str,
-               debt_amount: str) -> Iterable[Debt]:
-        result: Iterable[Debt] = Debt.objects.filter(first_name=first_name,
-                                                     last_name=last_name,
-                                                     debt_amount=debt_amount)
+    def filter(self, **fields: Dict[str, Union[str, int]]) -> Iterable[Debt]:
+        result: Iterable[Debt] = Debt.objects.filter(**fields)
         return result
 
 
 @dataclasses.dataclass
 class FakeDebtRepository:
-    objects: List[Debt] = []
+    _objects: List[Debt] = dataclasses.field(default_factory=list)
 
-    def create(self, first_name: str, last_name: str,
-               debt_amount: str) -> Debt:
+    def create(self, *, first_name: str, last_name: str,
+               debt_amount: int) -> Debt:
         x = Debt(first_name=first_name,
                  last_name=last_name,
                  debt_amount=debt_amount)
-        self.objects.append(x)
+        self._objects.append(x)
         return x
 
-    def _matching(self, obj: Debt, first_name: str, last_name: str,
-                  debt_amount: str) -> bool:
-        if obj.first_name != first_name:
-            return False
-        if obj.last_name != last_name:
-            return False
-        if obj.debt_amount != debt_amount:
-            return False
+    def _matching(self, obj: Debt,
+                  **fields: Dict[str, Union[str, int]]) -> bool:
+        for key, value in fields.items():
+            if getattr(obj, key) != value:
+                return False
         return True
 
-    def filter(self, first_name: str, last_name: str,
-               debt_amount: str) -> Iterable[Debt]:
+    def filter(self, **fields: Dict[str, Union[str, int]]) -> Iterable[Debt]:
         matching_debts = []
-        for obj in self.objects:
-            if self._matching(obj, first_name, last_name, debt_amount):
+        for obj in self._objects:
+            if self._matching(obj, **fields):
                 matching_debts.append(obj)
         return matching_debts
---
+++ debt_site/tests/test_repositories.py
@@ -0,0 +1,59 @@
+from typing import NoReturn
+
+import pytest
+
+from debt_site.repositories import DebtRepository, FakeDebtRepository
+
+
+@pytest.mark.parametrize(
+    'debt_repository_class',
+    [  # type: ignore
+        DebtRepository,
+        FakeDebtRepository,
+    ])
+@pytest.mark.django_db  # type: ignore
+def test_debt_repository_create(debt_repository_class) -> NoReturn:
+    repo = debt_repository_class()
+    first_name, last_name, debt_amount = "Danya", "Fedyaev", 7000
+    created = repo.create(first_name=first_name,
+                          last_name=last_name,
+                          debt_amount=debt_amount)
+    filtered = repo.filter(first_name=first_name,
+                           last_name=last_name,
+                           debt_amount=debt_amount)
+    assert len(filtered) == 1
+    assert created == filtered[0]
+
+
+@pytest.mark.parametrize(
+    'debt_repository_class',
+    [  # type: ignore
+        DebtRepository,
+        FakeDebtRepository,
+    ])
+@pytest.mark.django_db  # type: ignore
+def test_debt_repository_filter_on_empty_db(debt_repository_class) -> NoReturn:
+    repo = debt_repository_class()
+    filtered = repo.filter()
+    assert len(filtered) == 0
+
+
+@pytest.mark.parametrize(
+    'debt_repository_class',
+    [  # type: ignore
+        DebtRepository,
+        FakeDebtRepository,
+    ])
+@pytest.mark.django_db  # type: ignore
+def test_debt_repository_filter(debt_repository_class) -> NoReturn:
+    repo = debt_repository_class()
+    first_name, last_name, debt_amount = "Danya", "Fedyaev", 10000
+    created = repo.create(first_name=first_name,
+                          last_name=last_name,
+                          debt_amount=debt_amount)
+    repo.create(first_name="Vitya", last_name="Voronin", debt_amount=15000)
+    filtered = repo.filter(first_name=first_name,
+                           last_name=last_name,
+                           debt_amount=debt_amount)
+    assert len(filtered) == 1
+    assert created == filtered[0]
